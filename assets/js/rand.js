d3e3833a0074ff6cf05c0560896faddf39a14189
Carl Johnson <me@carlmjohnson.net>
2023-08-23 20:54:09 -0400

    WIP initial version

---
+++ assets/js/rand.js
@@ -0,0 +1,101 @@
+function secureRand(min, max) {
+  let range = max - min;
+  if (range < 2) {
+    return min;
+  }
+
+  let bits_needed = Math.ceil(Math.log2(range));
+  if (bits_needed > 31) {
+    throw new Error("cannot generate numbers larger than 31 bits.");
+  }
+
+  let mask = (1 << bits_needed) - 1;
+
+  let start = window.performance.now();
+  const maxTime = 100; // 100ms
+
+  // Create byte array
+  let byteArray = new Uint32Array(1);
+  while (true) {
+    // fill it with random numbers
+    window.crypto.getRandomValues(byteArray);
+
+    let rval = bigEndian(byteArray);
+
+    // Use & to apply the mask and reduce the number of lookups
+    rval = rval & mask;
+
+    if (rval < range) {
+      // Return an integer that falls within the range
+      return min + rval;
+    }
+    // Integer out of acceptable range
+    if (window.performance.now() - start > maxTime) {
+      throw new Error("took too long generating random number");
+    }
+  }
+}
+
+function bigEndian(byteArray) {
+  return new DataView(byteArray.buffer).getUint32();
+}
+
+function rando() {
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const digits = "1234567890";
+
+  return {
+    requirements: [
+      lower,
+      upper,
+      digits,
+    ],
+
+    alphabet: lower + upper + digits,
+
+    length: 10,
+
+    password: "",
+    error: "",
+
+    init() {
+      this.genPass()
+    },
+
+    genPass() {
+      let password = "";
+
+      const maxTime = 100; // 100ms
+      let start = window.performance.now();
+
+      while (!this.meetsRequirements(password)) {
+        if (window.performance.now() - start > maxTime) {
+          this.error = "Could not find a password meeting requirements";
+          return;
+        }
+
+        let chars = [];
+        for (let i = 0; i < this.length; i++) {
+          let choiceIdx = secureRand(0, this.alphabet.length);
+          chars.push(this.alphabet[choiceIdx]);
+        }
+        password = chars.join('');
+      }
+
+      this.password = password;
+    },
+
+    meetsRequirements(pwd) {
+      return this.requirements.every(req => this.containsAny(pwd, req))
+    },
+
+    containsAny(str, chars) {
+      return Array.from(str).some(c => chars.includes(c));
+    }
+  }
+}
+
+document.addEventListener('alpine:init', () => {
+  Alpine.data('rando', rando);
+});
